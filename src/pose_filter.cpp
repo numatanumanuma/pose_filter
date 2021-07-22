de277dea92688cb8d712e39a5597a6ab6bfcc4bb
yuisouth <minami-y@roboken.iit.tsukuba.ac.jp>
2021-07-22 14:24:21 +0900

    update

--- src/pose_filter.cpp
+++ src/pose_filter.cpp
@@ -8,11 +8,18 @@ PoseFilter::PoseFilter(){
     nh.param("sub_topic",       sub_topic_,     std::string("ndt_pose"));
     pose_pub_ = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>(pub_topic_, 10);
     pose_sub_ = nh.subscribe(sub_topic_, 1, &PoseFilter::msgsCallback, this);
-    timer_ = nh.createTimer(ros::Duration(0.5), &PoseFilter::timerCallback, this);
+    timer_ = nh.createTimer(ros::Duration(0.05), &PoseFilter::timerCallback, this);
 
-    pose_.position.x = 0.0;
-    pose_.position.y = 0.0;
-    pose_.position.z = 0.0;
+    pose_.position.x        = 0.0;
+    pose_.position.y        = 0.0;
+    pose_.position.z        = 0.0;
+    new_pose_.position.x    = 0.0;
+    new_pose_.position.y    = 0.0;
+    new_pose_.position.z    = 0.0;
+    new_pose_.orientation.x = 0.0;
+    new_pose_.orientation.y = 0.0;
+    new_pose_.orientation.z = 0.0;
+    new_pose_.orientation.w = 1.0;
     
 }
 
@@ -24,17 +31,15 @@ void PoseFilter::msgsCallback(const geometry_msgs::PoseStamped::ConstPtr& msgs){
     pose_ = msgs->pose;
     new_pose_.position.x = pose_.position.x;
     new_pose_.position.y = pose_.position.y;
-    new_pose_.position.z = 0;
-    double roll, pitch, yaw;
+    new_pose_.position.z = pose_.position.z;
     new_pose_.orientation = pose_.orientation;
 
     publishPose();
-    broadcastFrame();
-
 }
 
 void PoseFilter::timerCallback(const ros::TimerEvent&){
-    ROS_INFO("x:%0.3f, y:%0.3f", pose_.position.x, pose_.position.y);
+    broadcastFrame();
+    // ROS_INFO("x:%0.3f, y:%0.3f", pose_.position.x, pose_.position.y);
 }
 
 void PoseFilter::publishPose(){
@@ -45,23 +50,29 @@ void PoseFilter::publishPose(){
     pose_msg_.header.stamp = stamp;
     pose_msg_.header.frame_id = global_frame_;
     pose_msg_.pose.pose = new_pose_;
+    // double eye[36]{};
+    // for(int i = 0; i < 36; i += 7)
+        // pose_msg_.pose.covariance[i] = 1.0;
+    // pose_msg_.pose.covariance = eye;
     pose_pub_.publish(pose_msg_);
     ros::Duration(0.01).sleep();
 }
 
 void PoseFilter::broadcastFrame(){
-    static tf::TransformBroadcaster br;
-	tf::Transform transform;
-    tf::Quaternion quat;
+    static tf::TransformBroadcaster br{};
+	tf::Transform transform{};
+    tf::Transform new_transform{};
+    tf::Quaternion quat(0.0, 0.0, 0.0, 1.0);
+    tf::Quaternion new_quat;
+
+    // 子フレームから親フレームの順でpublishするべき
+    // odom -> base_link
+	new_transform.setOrigin(tf::Vector3(new_pose_.position.x, new_pose_.position.y, new_pose_.position.z));
+    quaternionMsgToTF(new_pose_.orientation, new_quat);
+	new_transform.setRotation(new_quat);
+	br.sendTransform(tf::StampedTransform(new_transform, ros::Time::now(), "odom", base_frame_));
 
     // map -> odom
-    transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
-	transform.setRotation(quat);
+    transform.setRotation(quat);
 	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), global_frame_, "odom"));
-
-    // odom -> base_link
-	transform.setOrigin(tf::Vector3(new_pose_.position.x, new_pose_.position.y, 0.0));
-    quaternionMsgToTF(new_pose_.orientation, quat);
-	transform.setRotation(quat);
-	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", base_frame_));
 }
