dd27261ebc8fa3a13be219464b8330801a2e5460
numatanumanuma <chiat18re@icloud.com>
2021-10-28 16:14:07 +0900

    Update

--- src/pose_filter.cpp
+++ src/pose_filter.cpp
@@ -16,12 +16,9 @@ void PoseFilter::msgsCallback(const geometry_msgs::PoseStamped::ConstPtr& msgs){
     geometry_msgs::PoseWithCovarianceStamped new_msg{};
     new_msg.header = msgs->header;
     new_msg.header.frame_id = global_frame_;
-    new_msg.pose.pose = msgs->pose;
-    new_msg.pose.pose.position.z = 0;
     for(int i = 0; i < 36; i += 7){
         new_msg.pose.covariance[i] = 1.0;
     }
-    pose_pub_.publish(new_msg);
 
     tf::Transform tf_odom_base{};
     tf_odom_base.setOrigin(tf::Vector3(
@@ -30,7 +27,37 @@ void PoseFilter::msgsCallback(const geometry_msgs::PoseStamped::ConstPtr& msgs){
         0
     ));
     tf::Quaternion quat_odom_base;
-    quaternionMsgToTF(msgs->pose.orientation, quat_odom_base);
+
+    tf::Quaternion pose_quat_before(
+        msgs->pose.orientation.x,
+        msgs->pose.orientation.y,
+        msgs->pose.orientation.z,
+        msgs->pose.orientation.w
+    );
+
+    pose_quat_before.normalize();
+ 
+    tf::Vector3 axis(0,0,1);
+    double roll, pitch, yaw;
+    tf::Matrix3x3(pose_quat_before).getRPY(roll, pitch, yaw);
+    tf::Quaternion pose_quat_after(axis, yaw);
+
+
+    geometry_msgs::Quaternion qmsg;
+    qmsg.x = pose_quat_after.getX();
+    qmsg.y = pose_quat_after.getY();
+    qmsg.z = pose_quat_after.getZ();
+    qmsg.w = pose_quat_after.getW();
+    quaternionMsgToTF(qmsg, quat_odom_base);
+
+
+    new_msg.pose.pose.position.x = msgs->pose.position.x;
+    new_msg.pose.pose.position.y = msgs->pose.position.y;
+    new_msg.pose.pose.position.z = 0;
+    new_msg.pose.pose.orientation = qmsg;
+    pose_pub_.publish(new_msg);
+
+    // quaternionMsgToTF(msgs->pose.orientation, quat_odom_base);
     tf_odom_base.setRotation(quat_odom_base);
     br.sendTransform(tf::StampedTransform(tf_odom_base, msgs->header.stamp, "odom", base_frame_));
 
